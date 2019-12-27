f0c0daef9836b707277295458d06d5e95481120e
fanzou2020 <fanzou2020@icloud.com>
2019-12-27 13:41:47 -0500

    Initial commit

---
+++ Sort/src/sort.java
@@ -0,0 +1,8 @@
+import edu.princeton.cs.algs4.StdOut;
+public class sort {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        StdOut.println("best");
+    }
+}
